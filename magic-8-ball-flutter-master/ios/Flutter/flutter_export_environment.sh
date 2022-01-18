51ec78c626652ff61ffc5a832899edf860be2921
kinjal gajjar <gajjarkinjalbhailalbhai@gmail.com>
2022-01-18 17:49:09 +0530

    upadate

--- magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
+++ magic-8-ball-flutter-master/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Kinjal\Documents\flutter\examples\magic-8-ball-flutter-master"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Kinjal\Documents\flutter\flutterexamples\magic-8-ball-flutter-master"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
