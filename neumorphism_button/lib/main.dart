8337c263217132aa96ba51faf695cccbaa6fcc47
kinjal gajjar <gajjarkinjalbhailalbhai@gmail.com>
2022-02-07 18:55:39 +0530

    nenumorphism_button

---
+++ neumorphism_button/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  bool _isElevated = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: Center(
+        child: GestureDetector(
+          onTap: () {
+            setState(() {
+              _isElevated = !_isElevated;
+            });
+          },
+          child: AnimatedContainer(
+            duration: Duration(microseconds: 200),
+            height: 200,
+            width: 200,
+            decoration: BoxDecoration(
+                color: Colors.grey[300],
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: _isElevated
+                    ? [
+                        BoxShadow(
+                          color: Colors.grey[500]!,
+                          offset: const Offset(4, 4),
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        ),
+                        const BoxShadow(
+                          color: Colors.white,
+                          offset: const Offset(-4, -4),
+                          blurRadius: 15,
+                          spreadRadius: 1,
+                        )
+                      ]
+                    : null),
+          ),
+        ),
+      ),
+    );
+  }
+}
